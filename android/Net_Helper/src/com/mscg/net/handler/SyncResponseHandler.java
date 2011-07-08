d6fb70a56370ec8f7a61ad0bead05ba6b30d29b8
giuseppemiscione <giuseppemiscione@gmail.com>
2011-07-08 14:40:21 +0000

    List type selection

--- android/Net_Helper/src/com/mscg/net/handler/SyncResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.mscg.net.handler;
-
-import org.apache.http.client.ResponseHandler;
-
-public interface SyncResponseHandler<T> extends ResponseHandler<T> {
-
-    public void startDownload();
-
-}
